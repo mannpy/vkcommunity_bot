1fe751311b3ac17c21deaf2aaee56e37704c4553
Rustam S <info@life2film.com>
2016-11-24 16:07:10 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+vk={}
+
+vk['bro']={
+    'peer_id' : '-8888888888',
+    'access_token' : 'bro'}
+
+mysql={}
+
+mysql['bro']={
+    'host':"db.bro.com",
+    'user':"bro",
+    'passwd':"bro",
+    'base':"bro"
+}
